e1c867c4f63237bc7c55b42e21e2024cb579eaca
dbwnsgur741 <dbwnsgur741@gmail.com>
2021-01-25 18:25:28 +0900

    branch Test

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,28 @@
 <script>
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { setUserId } from './plugins/googleAnalyics.js'
 
 export default defineComponent({
   name: 'App',
   components: {
     IonApp,
     IonRouterOutlet
+  },
+  created(){
+    var randNum = parseInt(Math.random() * 100);
+    setUserId(randNum);
+  },
+
+  watch:{
+    '$route'(to,from){
+      console.log(this.$route.path);
+      var beforeUrl = from;
+      var toUrl = to;
+
+      console.log("BeforeURL : " + beforeUrl);
+      console.log("ToURL : " + toUrl);
+    }
   }
 });
 </script>
